6cf3ceaf7d5326061d0fd912de8f9564a4426ec6
sukam09 <sukam09@naver.com>
2022-07-07 16:56:30 +0900

    feat: add hash routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import Header from './components/Header';
+import { Routes, Route, HashRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SingUp';
+import Settings from './pages/Settings';
+import NewArticle from './pages/NewArticle';
 
 export default function App() {
-  return <Header />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/editor" element={<NewArticle />} />
+      </Routes>
+    </HashRouter>
+  );
 }
