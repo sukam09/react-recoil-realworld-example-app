9349b22b5d23ebc3853c0d017222c89949419c97
sukam09 <sukam09@naver.com>
2022-07-11 16:49:06 +0900

    chore: modify react function component definition to arrow function

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,13 @@ module.exports = {
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
     ],
     '@typescript-eslint/no-use-before-define': 'off',
+    'no-console': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+      },
+    ],
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
