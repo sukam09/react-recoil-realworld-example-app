eedd09808440a37e41d973315753595ee60a57f4
sukam09 <sukam09@naver.com>
2022-07-15 17:15:24 +0900

    feat: implement user api

---
+++ src/api/user.ts
@@ -0,0 +1,18 @@
+import api from "./api";
+
+interface LoginBody {
+  email: string;
+  password: string;
+}
+
+interface UserBody extends LoginBody {
+  username: string;
+}
+
+export const postUser = async (url: string, body: { user: UserBody }) => {
+  return await api.post(url, body);
+};
+
+export const postUserLogin = async (url: string, body: { user: LoginBody }) => {
+  return await api.post(url, body);
+};
