225c6acd4d8e99556c9ebd5f0415e0c2c117512d
sukam09 <sukam09@naver.com>
2022-07-20 13:09:52 +0900

    refactor: merge interface to UserProps

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,25 +1,17 @@
 import api from "./api";
 
-interface LoginBody {
-  email: string;
-  password: string;
+interface UserProps {
+  user: {
+    username?: string;
+    email: string;
+    password: string;
+  };
 }
 
-interface UserBody extends LoginBody {
-  username: string;
-}
-
-export const postUser = async (url: string, body: { user: UserBody }) => {
+export const postUser = async (url: string, body: UserProps) => {
   return await api.post(url, body);
 };
 
-export const postUserLogin = async (url: string, body: { user: LoginBody }) => {
+export const postUserLogin = async (url: string, body: UserProps) => {
   return await api.post(url, body);
-};
-
-export const getUser = async (
-  url: string,
-  headers: { headers: { Authorization: string } }
-) => {
-  return await api.get(url, headers);
 };
