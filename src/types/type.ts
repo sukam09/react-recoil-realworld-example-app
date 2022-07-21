ab6c5fc5769c0bc57b7d5686645c92bcb33e9318
sukam09 <sukam09@naver.com>
2022-07-21 16:21:48 +0900

    feat: add BodyProps and HeaderProps

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,8 +1,27 @@
 export interface UserProps {
   email: string;
-  password: string;
+  password?: string;
   username?: string;
   bio?: string;
   image?: string;
   token?: string;
 }
+
+export interface BodyProps {
+  user: UserProps;
+}
+
+export interface HeaderProps {
+  Authorization: string;
+}
+
+// export interface ConfigProps {
+//   headers: HeaderProps;
+// }
+
+// export interface ErrorProps {
+//   email: string;
+//   password: string;
+//   username?: string;
+//   emailOrPassword?: string;
+// }
