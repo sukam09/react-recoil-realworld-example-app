7b67b1657efe04fd86e708dfb550bac2197ba1f7
sukam09 <sukam09@naver.com>
2022-07-22 10:32:16 +0900

    feat: add getUser API

--- src/api/user.ts
+++ src/api/user.ts
@@ -13,3 +13,7 @@ export const postUserLogin = async (url: string, body: BodyProps) => {
 export const putUser = async (url: string, body: BodyProps, config: any) => {
   return await api.put(url, body, config);
 };
+
+export const getUser = async (url: string, config: any) => {
+  return await api.get(url, config);
+};
