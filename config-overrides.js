6620480820376996808024f06e7de86ddd1b2356
sukam09 <sukam09@naver.com>
2022-07-27 09:53:24 +0900

    build: add path alias

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { override, addWebpackAlias } = require("customize-cra");
+const path = require("path");
+
+module.exports = override(
+  addWebpackAlias({
+    "@": path.resolve(__dirname, "src/"),
+  })
+);
