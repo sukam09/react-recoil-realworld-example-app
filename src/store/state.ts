56aac9eb7e197dac26975d8aaca986a40a02217d
sukam09 <sukam09@naver.com>
2022-07-27 10:02:55 +0900

    feat: add tokenState and loginState

--- src/store/state.ts
+++ src/store/state.ts
@@ -3,14 +3,26 @@ import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
-export const userState = atom({
-  key: "user",
-  default: {
-    email: "",
-    username: "",
-    bio: "",
-    image: "",
-    token: "",
-  },
+export const tokenState = atom({
+  key: "token",
+  default: "",
   effects_UNSTABLE: [persistAtom],
 });
+
+export const loginState = atom({
+  key: "login",
+  default: false,
+  effects_UNSTABLE: [persistAtom],
+});
+
+// export const userState = atom({
+//   key: "user",
+//   default: {
+//     email: "",
+//     username: "",
+//     bio: "",
+//     image: "",
+//     token: "",
+//   },
+//   effects_UNSTABLE: [persistAtom],
+// });
