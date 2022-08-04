3b372cf1308db8116fa61e99bc1f434dff53daeb
sukam09 <sukam09@naver.com>
2022-08-04 13:34:01 +0900

    refactor: remove recoil state

--- src/components/Feed/GlobalFeed.tsx
+++ src/components/Feed/GlobalFeed.tsx
@@ -1,36 +1,37 @@
 import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import Loading from "@/components/Loading";
 
 const GlobalFeed = () => {
   return (
-    <>
-      <div className="article-preview">
-        <div className="article-meta">
-          <Link to="/">
-            <img
-              src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon4661.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1658762585003"
-              alt="test"
-            />
-          </Link>
-          <div className="info">
-            <Link to="/" className="author">
-              Eric Simons
-            </Link>
-            <span className="date">January 20th</span>
-          </div>
-          <button
-            type="button"
-            className="btn btn-outline-primary btn-sm pull-xs-right"
-          >
-            <i className="ion-heart" /> 29
-          </button>
-        </div>
-        <Link to="/" className="preview-link">
-          <h1>How to build webapps that scale</h1>
-          <p>This is the description for the post.</p>
-          <span>Read more...</span>
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link to="/">
+          <img
+            src="https://opgg-static.akamaized.net/images/profile_icons/profileIcon4661.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1658762585003"
+            alt="test"
+          />
         </Link>
+        <div className="info">
+          <Link to="/" className="author">
+            Eric Simons
+          </Link>
+          <span className="date">January 20th</span>
+        </div>
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-sm pull-xs-right"
+        >
+          <i className="ion-heart" /> 29
+        </button>
       </div>
-    </>
+      <Link to="/" className="preview-link">
+        <h1>How to build webapps that scale</h1>
+        <p>This is the description for the post.</p>
+        <span>Read more...</span>
+      </Link>
+    </div>
   );
 };
 
