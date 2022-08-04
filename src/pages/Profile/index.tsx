9a12739df279119ee0dcfad60f96f9a29e82cdbd
sukam09 <sukam09@naver.com>
2022-08-04 11:07:02 +0900

    refactor: separate user profile part from profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,68 +1,11 @@
-import { useState, useEffect } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { useRecoilValue, useSetRecoilState } from "recoil";
 
-import { getProfile } from "@/api/profile";
-import { menuState, loginState } from "@/store/state";
-import { getUser } from "@/api/user";
-import { tokenState } from "@/store/state";
+import UserProfile from "@/components/UserProfile";
 
 const Profile = () => {
-  const [profile, setProfile] = useState({
-    image: "",
-    username: "",
-    bio: "",
-    following: false,
-  });
-  const { image, username, bio, following } = profile;
-  const [loginUsername, setLoginUsername] = useState("");
   const { userId } = useParams();
-  const token = useRecoilValue(tokenState);
-  const setMenu = useSetRecoilState(menuState);
-  const setLogin = useSetRecoilState(loginState);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const initProfile = async () => {
-      try {
-        const data = await (await getProfile(`/profiles/${userId}`)).data;
-        const profileData = data.profile;
-        setProfile({
-          // image: profileData.image,
-          image:
-            "https://opgg-static.akamaized.net/images/profile_icons/profileIcon4661.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1658762585003",
-          username: profileData.username,
-          bio: profileData.bio,
-          following: profileData.following,
-        });
-      } catch (error: any) {
-        navigate("/", { replace: true });
-      }
-    };
-    initProfile();
-  }, [userId, navigate]);
-
-  useEffect(() => {
-    const getLoginUsername = async () => {
-      try {
-        const data = await (
-          await getUser("/user", {
-            headers: {
-              Authorization: `Token ${token}`,
-            },
-          })
-        ).data;
-        setLoginUsername(data.user.username);
-      } catch (error: any) {
-        setLogin(false);
-        localStorage.clear();
-        navigate("/", { replace: true });
-      }
-    };
-    getLoginUsername();
-    username === loginUsername ? setMenu(5) : setMenu(6);
-  }, [loginUsername, setMenu, token, username, navigate, setLogin]);
+  const username = "sukam09"; // temporary
 
   return (
     <>
@@ -73,22 +16,7 @@ const Profile = () => {
       </HelmetProvider>
 
       <div className="profile-page">
-        <div className="user-info">
-          <div className="container">
-            <div className="row">
-              <div className="col-xs-12 col-md-10 offset-md-1">
-                <img src={image} className="user-img" alt="profile" />
-                <h4>{username}</h4>
-                <p>{bio}</p>
-                <Link to="/settings">
-                  <button className="btn btn-sm btn-outline-secondary action-btn">
-                    <i className="ion-gear-a"></i> Edit Profile Settings
-                  </button>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
+        <UserProfile />
 
         <div className="container">
           <div className="row">
