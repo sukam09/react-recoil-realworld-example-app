21963a62afe14542eae8d5c080b7ed41c5282aec
sukam09 <sukam09@naver.com>
2022-08-09 16:23:31 +0900

    feat: add article route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
 import Home from "@/pages/Home";
 import Login from "@/pages/Login";
 import Register from "@/pages/Register";
@@ -8,7 +10,7 @@ import Settings from "@/pages/Settings";
 import Editor from "@/pages/Editor";
 import Profile from "@/pages/Profile";
 import Favorites from "@/pages/Profile/Favorites";
-import Footer from "@/components/Footer";
+import Article from "@/pages/Article";
 
 import PrivateRoute from "@/utils/PrivateRoute";
 import NotFound from "@/utils/NotFound";
@@ -24,6 +26,7 @@ const App = () => {
           <Route path="/register" element={<Register />} />
           <Route path="/profile/:userId" element={<Profile />} />
           <Route path="/profile/:userId/favorites" element={<Favorites />} />
+          <Route path="/article/:slug" element={<Article />} />
           <Route element={<PrivateRoute />}>
             <Route path="/settings" element={<Settings />} />
             <Route path="/editor" element={<Editor />} />
