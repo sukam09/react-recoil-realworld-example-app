7d12e40c201e72a637960529caf29e52918d30ee
sukam09 <sukam09@naver.com>
2022-08-09 10:59:53 +0900

    feat: implement posting new article

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,11 +1,115 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 
-import { menuState } from "@/store/state";
+import { menuState, tokenState, loginState } from "@/store/state";
+import { postArticles } from "@/api/article";
+import RemovableTag from "@/components/Tag/RemovableTag";
+
+interface EditorProps {
+  title: string;
+  description: string;
+  body: string;
+  tag: string;
+  tagList: string[];
+}
 
 const Editor = () => {
+  const [editor, setEditor] = useState<EditorProps>({
+    title: "",
+    description: "",
+    body: "",
+    tag: "",
+    tagList: [],
+  });
+  const { title, description, body, tag, tagList } = editor;
+  const [error, setError] = useState({
+    title: "",
+    description: "",
+    body: "",
+  });
+  const [disabled, setDisabled] = useState(false);
+
+  const token = useRecoilValue(tokenState);
   const setMenu = useSetRecoilState(menuState);
+  const setLogin = useSetRecoilState(loginState);
+
+  const navigate = useNavigate();
+
+  const onChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    setEditor({
+      ...editor,
+      [name]: value,
+    });
+  };
+
+  const onEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      if (!tagList.includes(tag)) addTag(tag);
+    }
+  };
+
+  const addTag = (newTag: string) => {
+    setEditor({
+      ...editor,
+      tag: "",
+      tagList: [...tagList, newTag],
+    });
+  };
+
+  const removeTag = (target: string) => {
+    setEditor({ ...editor, tagList: tagList.filter((tag) => tag !== target) });
+  };
+
+  const publishArticle = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setDisabled(true);
+    try {
+      const data = await (
+        await postArticles(
+          "/articles",
+          {
+            article: {
+              title: title,
+              description: description,
+              body: body,
+              tagList: tagList,
+            },
+          },
+          {
+            headers: {
+              Authorization: `Token ${token}`,
+            },
+          }
+        )
+      ).data;
+      const slug = data.article.slug;
+      navigate(`/article/${slug}`);
+    } catch (error: any) {
+      // console.log(error);
+      const errorMessage = error.response.data.errors;
+      const statusCode = error.response.status;
+      if (statusCode === 422) {
+        setError({
+          title: errorMessage.title,
+          description: errorMessage.description,
+          body: errorMessage.body,
+        });
+      } else {
+        // 404
+        // TODO: may be separated to react hooks?
+        setLogin(false);
+        localStorage.clear();
+        navigate("/", { replace: true });
+      }
+    }
+    setDisabled(false);
+  };
 
   useEffect(() => {
     setMenu(3);
@@ -23,13 +127,23 @@ const Editor = () => {
         <div className="container page">
           <div className="row">
             <div className="col-md-10 offset-md-1 col-xs-12">
-              <form>
+              <ul className="error-messages">
+                {error.title && <li>title {error.title}</li>}
+                {error.description && <li>description can't be blank</li>}
+                {error.body && <li>body can't be blank</li>}
+              </ul>
+
+              <form onSubmit={(event) => publishArticle(event)}>
                 <fieldset>
                   <fieldset className="form-group">
                     <input
                       type="text"
                       className="form-control form-control-lg"
                       placeholder="Article Title"
+                      name="title"
+                      value={title}
+                      onChange={onChange}
+                      disabled={disabled}
                     />
                   </fieldset>
                   <fieldset className="form-group">
@@ -37,6 +151,10 @@ const Editor = () => {
                       type="text"
                       className="form-control"
                       placeholder="What's this article about?"
+                      name="description"
+                      value={description}
+                      onChange={onChange}
+                      disabled={disabled}
                     />
                   </fieldset>
                   <fieldset className="form-group">
@@ -44,6 +162,10 @@ const Editor = () => {
                       className="form-control"
                       rows={8}
                       placeholder="Write your article (in markdown)"
+                      name="body"
+                      value={body}
+                      onChange={onChange}
+                      disabled={disabled}
                     ></textarea>
                   </fieldset>
                   <fieldset className="form-group">
@@ -51,13 +173,26 @@ const Editor = () => {
                       type="text"
                       className="form-control"
                       placeholder="Enter tags"
+                      name="tag"
+                      value={tag}
+                      onChange={onChange}
+                      onKeyDown={onEnter}
+                      disabled={disabled}
                     />
                     <div className="tag-list"></div>
                   </fieldset>
-                  <button
-                    className="btn btn-lg pull-xs-right btn-primary"
-                    type="button"
-                  >
+
+                  <div>
+                    {tagList.map((tag) => (
+                      <RemovableTag
+                        key={tag}
+                        name={tag}
+                        onClick={() => removeTag(tag)}
+                      />
+                    ))}
+                  </div>
+
+                  <button className="btn btn-lg pull-xs-right btn-primary">
                     Publish Article
                   </button>
                 </fieldset>
