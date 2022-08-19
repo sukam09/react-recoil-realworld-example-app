150e9a6971fa354e59e24f5e42eee4c2ca7b8fbb
sukam09 <sukam09@naver.com>
2022-08-19 17:17:54 +0900

    ✨ feat: implement article preview component

---
+++ src/components/Article/ArticlePreview.tsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+import { ArticlePreviewProps } from "@/shared/type";
+import { TEST_IMAGE } from "@/shared/dummy";
+
+const ArticlePreview = ({
+  slug,
+  title,
+  description,
+  tagList,
+  createdAt,
+  favorited,
+  favoritesCount,
+  author: { username, image },
+}: ArticlePreviewProps) => {
+  return (
+    <div className="article-preview">
+      <div className="article-meta">
+        <Link to={`/profile/${username}`}>
+          {/* FIXME: API error
+          <img src={image} /> */}
+          <img src={TEST_IMAGE} />
+        </Link>
+        <div className="info">
+          <Link to={`/profile/${username}`} className="author">
+            {username}
+          </Link>
+          <span className="date">{createdAt}</span>
+        </div>
+        <button
+          type="button"
+          className="btn btn-outline-primary btn-sm pull-xs-right"
+        >
+          <i className="ion-heart" /> {favoritesCount}
+        </button>
+      </div>
+      <Link to={`/article/${slug}`} className="preview-link">
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <span>Read more...</span>
+      </Link>
+    </div>
+  );
+};
+
+export default ArticlePreview;
---
+++ src/components/Article/index.tsx
@@ -0,0 +1,1 @@
+export { default as ArticlePreview } from "@/components/Article/ArticlePreview";
