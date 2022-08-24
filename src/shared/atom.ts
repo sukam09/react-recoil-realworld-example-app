d016298d4158047ca4119bb4122559c63f34afb9
sukam09 <sukam09@naver.com>
2022-08-24 16:26:05 +0900

    ➖ build: remove recoil-persist

--- src/shared/atom.ts
+++ src/shared/atom.ts
@@ -1,19 +1,4 @@
 import { atom } from "recoil";
-import { recoilPersist } from "recoil-persist";
-
-const { persistAtom } = recoilPersist();
-
-export const tokenState = atom({
-  key: "token",
-  default: "",
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const loginState = atom({
-  key: "login",
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
 
 export const menuState = atom({
   key: "menu",
