9fee10b730a90ef155f7843b73ada1c6ae82f93a
sukam09 <sukam09@naver.com>
2022-09-01 16:41:39 +0900

    feat: add recoil states for user and login

--- src/store/state.ts
+++ src/store/state.ts
@@ -1,6 +1,22 @@
 import { atom } from "recoil";
 
 export const menuState = atom({
-  key: "menu",
+  key: "menuState",
   default: 0,
+});
+
+export const userState = atom({
+  key: "userState",
+  default: {
+    email: "",
+    token: "",
+    username: "",
+    bio: "",
+    image: "",
+  },
+});
+
+export const isLoggedInState = atom({
+  key: "isLoggedInState",
+  default: false,
 });
