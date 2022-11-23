30ab2a2f86e9cf08f20573ee536e61a31315ccaa
sukam09 <sukam09@naver.com>
2022-11-23 11:08:20 +0900

    fix: fix logged in state (local storage -> recoil state)

--- src/components/Routes/PrivateRoute.tsx
+++ src/components/Routes/PrivateRoute.tsx
@@ -1,7 +1,10 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import { isLoggedInState } from "../../state";
 
 const PrivateRoute = () => {
-  const isLoggedIn = localStorage.getItem("token");
+  const isLoggedIn = useRecoilValue(isLoggedInState);
   return isLoggedIn ? <Outlet /> : <Navigate to="/" />;
 };
 
