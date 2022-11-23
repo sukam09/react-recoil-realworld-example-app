8922b5f0f51b92af33431953e90de7851c82ad00
sukam09 <sukam09@naver.com>
2022-11-23 10:55:13 +0900

    refactor: separate EditAndDeleteButton to two components

---
+++ src/components/Article/Buttons/DeleteButton.tsx
@@ -0,0 +1,11 @@
+const DeleteButton = () => {
+  return (
+    <>
+      <button className="btn btn-sm btn-outline-danger">
+        <i className="ion-trash-a"></i> Delete Article
+      </button>
+    </>
+  );
+};
+
+export default DeleteButton;
---
+++ src/components/Article/Buttons/EditButton.tsx
@@ -0,0 +1,11 @@
+const EditButton = () => {
+  return (
+    <>
+      <button className="btn btn-sm btn-outline-secondary">
+        <i className="ion-edit"></i> Edit Article
+      </button>
+    </>
+  );
+};
+
+export default EditButton;
