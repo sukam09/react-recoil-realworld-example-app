bfbcd26de0707b02bc7b3a3a7d1b024f108c815e
sukam09 <sukam09@naver.com>
2022-11-25 14:02:04 +0900

    feat: add putArticles API

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,17 +1,20 @@
-import { GET, POST, DELETE } from "./config";
+import { GET, POST, DELETE, PUT } from "./config";
 
-export const postArticles = (
-  url: string,
-  body: {
-    article: {
-      title: string;
-      description: string;
-      body: string;
-      tagList: string[];
-    };
-  }
-) => POST(url, body);
+interface articleAPIBodyProps {
+  article: {
+    title: string;
+    description: string;
+    body: string;
+    tagList: string[];
+  };
+}
+
+export const postArticles = (url: string, body: articleAPIBodyProps) =>
+  POST(url, body);
 
 export const getArticles = (url: string) => GET(url);
 
 export const deleteArticles = (url: string) => DELETE(url);
+
+export const putArticles = (url: string, body: articleAPIBodyProps) =>
+  PUT(url, body);
