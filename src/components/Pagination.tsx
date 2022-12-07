439a9b89887cecb77e631368d957d954e78fe95d
sukam09 <sukam09@naver.com>
2022-12-07 17:29:36 +0900

    feat: add pagination

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+const Pagination = ({
+  page,
+  articlesCount,
+  movePage,
+}: {
+  page: number;
+  articlesCount: number;
+  movePage: (num: number) => void;
+}) => {
+  const pageLength = Math.floor(articlesCount / 10) + 1;
+  const pageNums = [...Array(pageLength).keys()];
+
+  if (pageLength === 1) return null;
+
+  return (
+    <>
+      <nav>
+        <ul className="pagination">
+          {pageNums.map((num) => (
+            <li
+              key={num}
+              className={`page-item ${page === num + 1 ? "active" : ""}`}
+            >
+              <Link
+                to="/"
+                className="page-link"
+                onClick={() => {
+                  if (page !== num + 1) movePage(num + 1);
+                }}
+              >
+                {num + 1}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Pagination;
--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -2,20 +2,31 @@ import { useState, useEffect } from "react";
 
 import Loading from "../Loading";
 import ArticlePreview from "../article/ArticlePreview";
+import Pagination from "../Pagination";
+
 import { getArticles } from "../../api/article";
 import { ArticleProps } from "../../types";
 
 const Feed = ({ query }: { query: string }) => {
   const [articles, setArticles] = useState<ArticleProps[]>([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [articlesCount, setArticlesCount] = useState(0);
+
+  const movePage = (num: number) => {
+    setPage(num);
+  };
 
   useEffect(() => {
     const initArticles = async () => {
-      const { articles } = await getArticles(`/articles${query}`);
+      const url =
+        query === "/feed" ? query : `${query}&offset=${10 * (page - 1)}`;
+      const { articles, articlesCount } = await getArticles(`/articles${url}`);
       setArticles(articles);
+      setArticlesCount(articlesCount);
     };
     initArticles().then(() => setLoading(false));
-  }, []);
+  }, [query, page]);
 
   return (
     <>
@@ -25,13 +36,23 @@ const Feed = ({ query }: { query: string }) => {
         </div>
       ) : (
         <>
-          {articles.length === 0 ? (
+          {articlesCount === 0 ? (
             <div className="article-preview">No articles are here... yet.</div>
           ) : (
             articles.map((article) => (
-              <ArticlePreview key={article.slug} article={article} />
+              <ArticlePreview
+                key={article.slug}
+                article={article}
+                isFavorites={false}
+                decreaseLength={undefined}
+              />
             ))
           )}
+          <Pagination
+            page={page}
+            articlesCount={articlesCount}
+            movePage={movePage}
+          />
         </>
       )}
     </>
