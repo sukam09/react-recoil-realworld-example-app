8db69813bd721d1e925dd067a2cf4d859fe7f8e1
sukam09 <sukam09@naver.com>
2022-12-12 16:56:11 +0900

    refactor: remove getUser API call

--- src/components/header/UserImage.tsx
+++ src/components/header/UserImage.tsx
@@ -1,51 +1,23 @@
-import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 
-import { getUser } from "../../api/user";
-import { menuState } from "../../state";
-import Loading from "../Loading";
+import { userState, menuState } from "../../state";
 
 const UserImage = () => {
-  const [userInfo, setUserInfo] = useState({
-    image: "",
-    username: "",
-  });
-  const { image, username } = userInfo;
-  const [loading, setLoading] = useState(true);
-
+  const user = useRecoilValue(userState);
   const menu = useRecoilValue(menuState);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const getUsername = async () => {
-      const data = await getUser("/user");
-      const { image, username } = data.user;
-      setUserInfo({
-        image: image,
-        username: username,
-      });
-    };
-    getUsername().then(() => setLoading(false));
-  }, [navigate]);
 
   return (
     <>
-      {loading ? (
-        <li className="nav-item">
-          <Loading />
-        </li>
-      ) : (
-        <li className="nav-item">
-          <Link
-            to={`/profile/${username}`}
-            className={`nav-link ${menu === 5 && "active"}`}
-          >
-            <img className="user-pic" src={image} />
-            {username}
-          </Link>
-        </li>
-      )}
+      <li className="nav-item">
+        <Link
+          to={`/profile/${user.username}`}
+          className={`nav-link ${menu === 5 && "active"}`}
+        >
+          <img className="user-pic" src={user.image} />
+          {user.username}
+        </Link>
+      </li>
     </>
   );
 };
