15bd305004502946f6d756394d3752e0b2c47cdb
sukam09 <sukam09@naver.com>
2022-12-22 16:30:05 +0900

    fix: remove useEffect

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
 import { Helmet, HelmetProvider } from "react-helmet-async";
