276742283dc9b5dd3f0290b5f74b2a44ac624dce
sukam09 <sukam09@naver.com>
2022-12-23 15:37:44 +0900

    fix: remove useEffect

--- src/components/common/FollowButton.tsx
+++ src/components/common/FollowButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { isLoggedInState } from "../../state";
