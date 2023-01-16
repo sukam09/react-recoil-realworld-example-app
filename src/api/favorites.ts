658f66f94edd61e3defa1b833f40726d2b78aceb
sukam09 <sukam09@naver.com>
2023-01-16 09:52:39 +0900

    style: add new line

--- src/api/favorites.ts
+++ src/api/favorites.ts
@@ -2,5 +2,6 @@ import { POST, DELETE } from "./config";
 
 export const postFavorites = (slug: string) =>
   POST(`/articles/${slug}/favorite`);
+
 export const deleteFavorites = (slug: string) =>
   DELETE(`/articles/${slug}/favorite`);
--- src/api/profile.ts
+++ src/api/profile.ts
@@ -1,7 +1,9 @@
 import { DELETE, GET, POST } from "./config";
 
 export const getProfile = (username: string) => GET(`/profiles/${username}`);
+
 export const postFollow = (username: string) =>
   POST(`/profiles/${username}/follow`);
+
 export const deleteFollow = (username: string) =>
   DELETE(`/profiles/${username}/follow`);
