258759019e668f8cb8f25ced91bee6d275e33e90
sukam09 <sukam09@naver.com>
2023-02-09 15:43:34 +0900

    feat: reactivate React.StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <React.StrictMode>
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </React.StrictMode>
 );
